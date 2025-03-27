ebf41054227d1c9a798da752e1b991d5caec716e
Riccardo Aquilanti <55596706+AquilaTM@users.noreply.github.com>
2025-03-27 13:21:06 +0100

    Add files via upload

---
+++ Sorting Algorithms/MergeSort.java
@@ -0,0 +1,59 @@
+public class MergeSort {
+    
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            } else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 27, 41, 32, 9, 82, 10, 90, 2, 12};
+        int n = arr.length;
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        mergeSort(arr, n);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
